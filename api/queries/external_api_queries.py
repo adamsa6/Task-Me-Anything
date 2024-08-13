e34a445d48fbf6753ee81f53da50d32e61b6da33
Andrew Major <major10707@gmail.com>
2024-08-13 18:08:16 +0000

    Created external api route and query for jokes

---
+++ api/queries/external_api_queries.py
@@ -0,0 +1,31 @@
+import requests
+import os
+
+API_NINJA_KEY = os.environ.get("API_NINJA_KEY")
+
+class GetExternalApi:
+
+    def get_joke(self):
+       
+        url =  "https://api.api-ninjas.com/v1/jokes"
+        headers = {"X-Api-Key": API_NINJA_KEY}
+        response = requests.get(url, headers=headers)
+        data = response.json()
+
+        if response.status_code == requests.codes.ok:
+            return data[0]["joke"]
+        else:
+            return {"Error": response.status_code, "Text": response.text}
+        
+
+    def get_quote(self):
+       
+        url =  "https://zenquotes.io/api/random"
+        response = requests.get(url)
+        data = response.json()
+
+        if response.status_code == requests.codes.ok:
+            return data[0]
+        else:
+            return {"Error": response.status_code, "Text": response.text}
+        
---
+++ api/routers/external_api_router.py
@@ -0,0 +1,32 @@
+from fastapi import (
+    Depends,
+    APIRouter,
+)
+from queries.external_api_queries import GetExternalApi
+from utils.authentication import try_get_jwt_user_data
+from models.users import UserResponse
+from utils.exceptions import user_exception
+
+
+router = APIRouter(prefix="/api")
+
+@router.get("/joke")
+def get_joke(
+    user: UserResponse = Depends(try_get_jwt_user_data),
+    queries: GetExternalApi = Depends(),
+):
+    if user is None:
+        raise user_exception
+    joke = queries.get_joke()
+    return joke
+
+
+@router.get("/quote")
+def get_quote(
+    user: UserResponse = Depends(try_get_jwt_user_data),
+    queries: GetExternalApi = Depends(),
+):
+    if user is None:
+        raise user_exception
+    quote = queries.get_quote()
+    return quote
