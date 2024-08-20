572f020f077f8ad4ab3ea234195c736983f6e2fc
Yazan <yzan.alsalhani@gmail.com>
2024-08-20 20:37:17 +0200

    signup form

--- ghi/src/components/SignInForm.jsx
+++ ghi/src/components/SignInForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Navigate } from 'react-router-dom'
+import { Navigate, Link } from 'react-router-dom'
 
 export default function SignInForm() {
     const [username, setUsername] = useState('')
@@ -9,22 +9,25 @@ export default function SignInForm() {
         e.preventDefault()
     }
     return (
-        <form onSubmit={handleFormSubmit}>
-            <input
-                type="text"
-                name="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Enter Username"
-            />
-            <input
-                type="text"
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Enter Password"
-            />
-            <button type="submit">Sign In</button>
-        </form>
+        <>
+            <form onSubmit={handleFormSubmit}>
+                <input
+                    type="text"
+                    name="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="Enter Username"
+                />
+                <input
+                    type="text"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Enter Password"
+                />
+                <button type="submit">Sign In</button>
+            </form>
+            <Link to="/signup">Create An Account</Link>
+        </>
     )
 }
