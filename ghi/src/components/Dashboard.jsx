5ad1efdd5e9f5928873a3716a1c024d634f83d8e
Yazan <yzan.alsalhani@gmail.com>
2024-08-23 21:39:18 +0200

    dashboard

--- ghi/src/components/Dashboard.jsx
+++ ghi/src/components/Dashboard.jsx
@@ -1,8 +1,7 @@
 import { useGetQuoteQuery } from '../app/api'
 import ListAssignedTasks from './ListAssignedTasks'
 import ListMyTasks from './ListMyTasks'
-
-
+import '../Dashboard.css'
 
 const Dashboard = () => {
     const { data: quotes, isLoading: quoteIsLoading } = useGetQuoteQuery()
@@ -11,24 +10,27 @@ const Dashboard = () => {
 
     return (
         <>
-            <div>
+            <div className="container">
                 <h1>Dashboard</h1>
-                <h2>Welcome!</h2>
-                {quotes.map((quote) => {
-                    return (
-                        <div key={quote.q}>
-                            <p>{quote.q}</p>
-                            <p>{quote.a}</p>
-                        </div>
-                    )
-                })}
-                <h1>My Top Priority Tasks</h1>
-                <div>
-                    <ListMyTasks isLimited={true} />
+                <div className="welcome-card">
+                    <h2>Welcome!</h2>
+                    {quotes.map((quote) => {
+                        return (
+                            <div key={quote.q} className="quote">
+                                <p>"{quote.q}"</p>
+                                <p>-{quote.a}</p>
+                            </div>
+                        )
+                    })}
                 </div>
-                <h1>Top Priority Tasks</h1>
-                <div>
-                    <ListAssignedTasks isLimited={true} />
+                <h1>My Top Priority Tasks</h1>
+                <div className="tasks-container">
+                    <div className="tasks-column">
+                        <ListMyTasks isLimited={true} />
+                    </div>
+                    <div className="tasks-column">
+                        <ListAssignedTasks isLimited={true} />
+                    </div>
                 </div>
             </div>
         </>
