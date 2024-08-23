345952e80e5d8cc14744afc227f610aaf2f3976c
Yazan <yzan.alsalhani@gmail.com>
2024-08-23 20:46:29 +0200

    my task

--- ghi/src/components/ListMyTasks.jsx
+++ ghi/src/components/ListMyTasks.jsx
@@ -1,26 +1,23 @@
 import { useEffect, useState } from 'react'
 import { useListMyTasksQuery } from '../app/api'
 import AssigneeTaskRow from './AssigneeTaskRow'
+import '../ListMyTasks.css'
 
-const ListMyTasks = ({isLimited}) => {
+const ListMyTasks = ({ isLimited }) => {
     const { data, isLoading } = useListMyTasksQuery()
     const [tasksToList, setTasksToList] = useState([])
 
     useEffect(() => {
         if (data) {
-            setTasksToList(
-                isLimited
-                    ? data.tasks.slice(0, 5)
-                    : data.tasks
-            )
+            setTasksToList(isLimited ? data.tasks.slice(0, 5) : data.tasks)
         }
     }, [data, isLimited])
 
     if (isLoading) return <>Loading...</>
 
     return (
-        <div>
-            <h2>Tasks I Have Created</h2>
+        <div className="container">
+            <h1>Tasks I Have Created</h1>
             <table>
                 <thead>
                     <tr>
@@ -32,7 +29,7 @@ const ListMyTasks = ({isLimited}) => {
                 </thead>
                 <tbody>
                     {tasksToList.map((task) => {
-                            return <AssigneeTaskRow key={task.id} task={task} />
+                        return <AssigneeTaskRow key={task.id} task={task} />
                     })}
                 </tbody>
             </table>
