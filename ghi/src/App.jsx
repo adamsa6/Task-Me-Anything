5773be259b0acc72d66a7c930675acb2613874e0
Arielle <arielleladams@gmail.com>
2024-08-26 15:01:00 -0400

    Removed Construct component

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
-
 import ErrorNotification from './components/ErrorNotification'
-import Construct from './components/Construct'
 
 import Nav from './components/Nav'
 
@@ -50,7 +48,6 @@ function App() {
             </header>
             <Outlet />
             <ErrorNotification error={error} />
-            {/* <Construct info={launchInfo} /> */}
         </div>
     )
 }
