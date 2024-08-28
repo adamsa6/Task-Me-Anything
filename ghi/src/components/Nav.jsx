578a692a9d773f76c1883d6d11628d1ce40a1311
Yazan <yzan.alsalhani@gmail.com>
2024-08-28 22:33:03 +0200

    made Nav Collapsable

--- ghi/src/components/Nav.jsx
+++ ghi/src/components/Nav.jsx
@@ -22,65 +22,74 @@ const Nav = () => {
                     alt="Logo"
                     style={{ width: '100px', height: 'auto' }}
                 />
-                <div className="d-flex flex-grow-1">
-                    <div className="d-flex flex-grow-1">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            {user && (
-                                <>
-                                    <li className="nav-item">
-                                        <NavLink
-                                            to="/dashboard"
-                                            className="nav-link btn btn-light"
-                                            end
-                                        >
-                                            Dashboard
-                                        </NavLink>
-                                    </li>
-                                    <li className="nav-item">
-                                        <NavLink
-                                            to="/tasks/mine"
-                                            className="nav-link btn btn-light"
-                                        >
-                                            My Created Tasks
-                                        </NavLink>
-                                    </li>
-                                    <li className="nav-item">
-                                        <NavLink
-                                            to="/assigned-tasks/mine"
-                                            className="nav-link btn btn-light"
-                                        >
-                                            My Assigned Tasks
-                                        </NavLink>
-                                    </li>
-                                    <li className="nav-item">
-                                        <NavLink
-                                            to="/tasks"
-                                            className="nav-link btn btn-light"
-                                            end
-                                        >
-                                            All Tasks
-                                        </NavLink>
-                                    </li>
-                                    <li className="nav-item">
-                                        <NavLink
-                                            to="/tasks/create"
-                                            className="nav-link btn btn-light"
-                                        >
-                                            Create Task
-                                        </NavLink>
-                                    </li>
-                                    <li className="nav-item">
-                                        <NavLink
-                                            to="/tasks/history"
-                                            className="nav-link btn btn-light"
-                                        >
-                                            Task History
-                                        </NavLink>
-                                    </li>
-                                </>
-                            )}
-                        </ul>
-                    </div>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        {user && (
+                            <>
+                                <li className="nav-item">
+                                    <NavLink
+                                        to="/dashboard"
+                                        className="nav-link btn btn-light"
+                                        end
+                                    >
+                                        Dashboard
+                                    </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink
+                                        to="/tasks/mine"
+                                        className="nav-link btn btn-light"
+                                    >
+                                        My Created Tasks
+                                    </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink
+                                        to="/assigned-tasks/mine"
+                                        className="nav-link btn btn-light"
+                                    >
+                                        My Assigned Tasks
+                                    </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink
+                                        to="/tasks"
+                                        className="nav-link btn btn-light"
+                                        end
+                                    >
+                                        All Tasks
+                                    </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink
+                                        to="/tasks/create"
+                                        className="nav-link btn btn-light"
+                                    >
+                                        Create Task
+                                    </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink
+                                        to="/tasks/history"
+                                        className="nav-link btn btn-light"
+                                    >
+                                        Task History
+                                    </NavLink>
+                                </li>
+                            </>
+                        )}
+                    </ul>
                     {user && (
                         <div className="d-flex">
                             <button
--- ghi/src/trash.jsx
+++ ghi/src/trash.jsx
@@ -1,72 +1,106 @@
-// .signin-container {
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-//     min-height: 100vh;
-//     /* background-color: #d11414; Adjust the background color as needed */
-//     padding: 100px;
-//     gap: 10%; /* Increase space between the two cards */
-//     width: 100%;
+// return (
+//     <nav className="navbar navbar-expand-lg navbar-light fixed-top custom-navbar">
+//         <div className="container-fluid">
+//             <img
+//                 src="/logo.png"
+//                 alt="Logo"
+//                 style={{ width: '100px', height: 'auto' }}
+//             />
+//             <div className="d-flex flex-grow-1">
+//                 <div className="d-flex flex-grow-1">
+//                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//                         {user && (
+//                             <>
+//                                 <li className="nav-item">
+//                                     <NavLink
+//                                         to="/dashboard"
+//                                         className="nav-link btn btn-light"
+//                                         end
+//                                     >
+//                                         Dashboard
+//                                     </NavLink>
+//                                 </li>
+//                                 <li className="nav-item">
+//                                     <NavLink
+//                                         to="/tasks/mine"
+//                                         className="nav-link btn btn-light"
+//                                     >
+//                                         My Created Tasks
+//                                     </NavLink>
+//                                 </li>
+//                                 <li className="nav-item">
+//                                     <NavLink
+//                                         to="/assigned-tasks/mine"
+//                                         className="nav-link btn btn-light"
+//                                     >
+//                                         My Assigned Tasks
+//                                     </NavLink>
+//                                 </li>
+//                                 <li className="nav-item">
+//                                     <NavLink
+//                                         to="/tasks"
+//                                         className="nav-link btn btn-light"
+//                                         end
+//                                     >
+//                                         All Tasks
+//                                     </NavLink>
+//                                 </li>
+//                                 <li className="nav-item">
+//                                     <NavLink
+//                                         to="/tasks/create"
+//                                         className="nav-link btn btn-light"
+//                                     >
+//                                         Create Task
+//                                     </NavLink>
+//                                 </li>
+//                                 <li className="nav-item">
+//                                     <NavLink
+//                                         to="/tasks/history"
+//                                         className="nav-link btn btn-light"
+//                                     >
+//                                         Task History
+//                                     </NavLink>
+//                                 </li>
+//                             </>
+//                         )}
+//                     </ul>
+//                 </div>
+//                 {user && (
+//                     <div className="d-flex">
+//                         <button
+//                             className="btn signout-button me-2"
+//                             onClick={() => {
+//                                 signout()
+//                             }}
+//                         >
+//                             Sign Out
+//                         </button>
+//                     </div>
+//                 )}
+//             </div>
+//         </div>
+//     </nav>
+// )
+
+// .custom-navbar {
+//     background-color: #003135;
 // }
 
-// .card {
-//     background-color: #ffffff;
-//     border-radius: 8px;
-//     box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
-//     padding: 30px;
-//     width: 100%;
-//     max-width: 500px; /* Increase the maximum width of the card */
+// .nav-link {
+//     color: #fff;
 // }
 
-// .card-info {
-//     display: flex;
-//     flex-direction: column;
-//     justify-content: center;
-//     align-items: center;
-//     text-align: center;
-//     width: 1500px;
+// .nav-link.active {
+//     background-color: #b2dbd1;
+//     color: #003135;
 // }
 
-// .card-info h1,
-// .card-info p {
-//     margin: 0;
-//     color: #0fa4af; /* Adjust color as needed */
-// }
-
-// .card-info p {
-//     margin-top: 10px;
-//     color: #333; /* Adjust color as needed */
-// }
-
-// .card-form {
-//     display: flex;
-//     flex-direction: column;
-// }
-
-// .card-form input,
-// .card-form button {
-//     margin-bottom: 15px; /* Increase margin between form elements */
-// }
-
-// .card-form input {
-//     padding: 15px;
-//     border: 1px solid #ddd;
-//     border-radius: 4px;
-// }
-
-// .card-form button {
-//     padding: 15px;
+// .signout-button {
+//     background-color: #964734;
 //     border: none;
-//     border-radius: 4px;
-//     background-color: #024950;
-//     color: #ffffff;
-//     cursor: pointer;
+//     color: #fff;
 // }
 
-// .card-form button:hover {
-//     background-color: #023740; /* Darker shade for hover effect */
-// }
-
-// .card-form a {
-//     color: #024950;
+// .signout-button:hover {
+//     background-color: #7a3a2c;
 // }
